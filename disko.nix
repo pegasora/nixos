8cf1431fdbf2413a3350221b5d3ae486b14c8a15
pegasora <dawsonhburgess@gmail.com>
2025-09-15 08:50:38 -0700

    removed hardware config - it gets generated. added disko.nix

---
+++ disko.nix
@@ -0,0 +1,41 @@
+{ disk ? "nvme0n1", ... }:
+
+# for dual boot with windows
+{
+  disko.devices = {
+    disk = {
+      ${disk} = {
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            windows = {
+              size = "256G";
+              content = {
+                type = "filesystem";
+                format = "ntfs";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
