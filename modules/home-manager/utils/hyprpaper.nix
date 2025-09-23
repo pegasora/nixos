3de58a165f7761b3c39595eb963f9a67e6883a11
pegasora <dawsonhburgess@gmail.com>
2025-09-22 18:19:09 -0700

    i think this should work

---
+++ modules/home-manager/utils/hyprpaper.nix
@@ -0,0 +1,9 @@
+{
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      preload = ["~/nixos/modules/home-manager/wm/hyprland/files/wallpapers/golden-gorge.jpg"];
+      wallpaper = ["~/nixos/modules/home-manager/wm/hyprland/files/wallpapers/golden-gorge.jpg"];
+    };
+  };
+}
