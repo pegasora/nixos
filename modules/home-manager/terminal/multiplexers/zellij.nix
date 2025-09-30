04be4a8f618a531a38caa02fc8b87598d919e758
pegasora <dawsonhburgess@gmail.com>
2025-09-30 00:51:12 -0700

    zellij

--- modules/home-manager/terminal/multiplexers/zellij.nix
+++ modules/home-manager/terminal/multiplexers/zellij.nix
@@ -1,1 +1,14 @@
-{}
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  programs.zellij = {
+    enable = true;
+    settings = {
+      default_shell = lib.getExe pkgs.fish;
+      theme = "catppuccin-mocha";
+      # show_startup_gips = false;
+    };
+  };
+}
